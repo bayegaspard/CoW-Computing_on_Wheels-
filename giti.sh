2ec69958861d6f435dd6eef0d8d9c7175ce0a085
baye <gaspybaye@gmail.com>
2021-01-27 16:34:09 +0300

    updated

---
+++ giti.sh
@@ -0,0 +1,7 @@
+echo "# CoW-Computing_on_Wheels-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BAYEGASPARD/CoW-Computing_on_Wheels-.git
+git push -u origin main
